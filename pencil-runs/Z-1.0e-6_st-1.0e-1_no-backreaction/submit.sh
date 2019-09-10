8221efdde9c205c973de6a86b039e1aaea7280d0
Vincent Carpenter <vscarpen@isaac1.bc.rzg.mpg.de>
2019-09-10 16:27:08 +0200

    updated run files

---
+++ pencil-runs/Z-1.0e-6_st-1.0e-1_no-backreaction/submit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Standard output and error:
+#SBATCH -o ./Z-1.0e-6_st-1.0e-1_nb.out.%j
+#SBATCH -e ./Z-1.0e-6_st-1.0e-1_nb.err.%j
+#SBATCH -D ./
+#SBATCH -J Z-1.0e-6_st-1.0e-1_nb
+#SBATCH --nodes=8
+#SBATCH --tasks-per-node=40
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=all
+#SBATCH --mail-user=carpenter@mpia.de
+# Wall clock limit
+#SBATCH --time=48:00:00
+
+pc_run -f $PENCIL_HOME/config/hosts/isaac/isaac1.bc.rzg.mpg.de.conf
